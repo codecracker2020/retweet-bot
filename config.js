372a226586b21a3af93386eba499fa7761045766
codecracker2020 <knaveenbabu@gmail.com>
2021-06-13 10:27:05 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+//Config Object 
+let config = {
+    consumer_key: "CONSUMER-KEY",
+    consumer_secret: "CONSUMER-SECRET",
+    access_token: "ACCESS_TOKEN",
+    access_token_secret: "TOKEN_SECRET"
+};
+//Export the Config to access from the main bot app.
+module.exports = config;
